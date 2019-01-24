4938db819dec2b02c27975b067ecc493624121a7
VLee <425773164@qq.com>
2019-01-24 23:42:50 +0800

    add

--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,28 @@
 module.exports = {
 	publicPath: './', //https://cli.vuejs.org/zh/config/#publicpath
-  assetsDir: 'static',
-  css: {
-    loaderOptions: {
-      stylus: {
-        'resolve url': true,
-        'import': [
-          './src/theme'
-        ]
-      }
-    }
-  },
-  pluginOptions: {
-    'cube-ui': {
-      postCompile: true,
-      theme: true
-    }
-  },
-  devServer: {
+	assetsDir: 'static',
+	css: {
+		loaderOptions: {
+			stylus: {
+				'resolve url': true,
+				'import': [
+					'./src/theme'
+				]
+			}
+		}
+	},
+	pluginOptions: {
+		'cube-ui': {
+			postCompile: true,
+			theme: true
+		}
+	},
+	devServer: {
 		proxy: { //如需跨域请求多个域名，在此对象进行扩展即可
 			'/api/chapter': {
 				target: 'http://chapter2.zhuishushenqi.com/',
 				ws: true,
-        changeOrigin: true,
+				changeOrigin: true,
 				pathRewrite: {
 					'^/api': ''
 				}
@@ -30,7 +30,7 @@ module.exports = {
 			'/api': {
 				target: 'http://api.zhuishushenqi.com/',
 				ws: true,
-        changeOrigin: true,
+				changeOrigin: true,
 				pathRewrite: {
 					'^/api': ''
 				}
